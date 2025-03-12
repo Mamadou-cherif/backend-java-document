c3ef1f8da66891aff8a9d8bf53a2e76581a265fc
Mamadou Cherif <barrymamadoucherif289@gmail.com>
2025-03-12 23:31:50 +0000

    le build qui va fonctionner

--- src/main/java/com/smshub/org/SmshubApplication.java
+++ src/main/java/com/smshub/org/SmshubApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class SmshubApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(SmshubApplication.class, args);
 	}
 }
