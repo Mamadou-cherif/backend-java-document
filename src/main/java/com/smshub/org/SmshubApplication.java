5bd4f2f60635f8020d32005e5840b9503ee059c0
Mamadou Cherif <barrymamadoucherif289@gmail.com>
2025-03-13 10:35:10 +0000

    new deployement

--- src/main/java/com/smshub/org/SmshubApplication.java
+++ src/main/java/com/smshub/org/SmshubApplication.java
@@ -2,7 +2,7 @@ package com.smshub.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing; 
 
 
 @SpringBootApplication
